c5e7984fe6ee060d2f2e150ac6c1b1d9839510fd
Ysj <brownstory82@gmail.com>
2026-04-07 03:05:43 +0900

    maven project

---
+++ src/main/java/com/example/danbook/DanbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.danbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/danbook/DanbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
