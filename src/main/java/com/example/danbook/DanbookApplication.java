567ab742492ad17b1cc4bab665389e0f2d687225
Ysj <brownstory82@gmail.com>
2026-04-07 03:14:12 +0900

    인텔리제이 환경 커밋 푸시 테스트

--- src/main/java/com/example/danbook/DanbookApplication.java
+++ src/main/java/com/example/danbook/DanbookApplication.java
@@ -3,6 +3,8 @@ package com.example.danbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// 인텔리제이 환경 테스트 04.07 여성제
 @SpringBootApplication
 public class DanbookApplication {
 
